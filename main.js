5761455f885c82d96ecdda852a7227ec21e9ff2f
shm_usuf <shmusuf@gmail.com>
2023-04-27 11:13:45 +0600

    logger middleware added

--- main.js
+++ main.js
@@ -1,22 +1,13 @@
-const { createStore, combineReducers } = require("redux");
+const { createStore, applyMiddleware } = require("redux");
+const { default: logger } = require("redux-logger");
 /* products constant */
 const GET_PRODUCTS = "GET_PRODUCTS";
 const ADD_PRODUCT = "ADD_PRODUCT";
-
-/* carts constant */
-const GET_CART_ITEMS = "GET_CART_ITEMS";
-const ADD_CART = "ADD_CART";
 
 //products state
 const initialProducts = {
   products: ["sugar", "salt"],
   numberOfProducts: 2,
-};
-
-//products state
-const initialCarts = {
-  cart: ["sugar"],
-  numberOfProducts: 1,
 };
 
 //products action
@@ -30,20 +21,6 @@ const addProduct = (product) => {
   return {
     type: ADD_PRODUCT,
     payload: product,
-  };
-};
-
-//cart action
-const getCart = () => {
-  return {
-    type: GET_CART_ITEMS,
-  };
-};
-
-const addCart = (value) => {
-  return {
-    type: ADD_CART,
-    payload: value,
   };
 };
 
@@ -66,35 +43,10 @@ const productReducer = (state = initialProducts, action) => {
   }
 };
 
-//cart reducer
-const cartReducer = (state = initialCarts, action) => {
-  switch (action.type) {
-    case GET_CART_ITEMS:
-      return {
-        ...state,
-      };
-
-    case ADD_CART:
-      return {
-        cart: [...state.cart, action.payload],
-        numberOfProducts: state.numberOfProducts + 1,
-      };
-
-    default:
-      return state;
-  }
-};
-
-const rootReducer = combineReducers({
-  productR: productReducer,
-  cartR: cartReducer,
-});
-
-const store = createStore(rootReducer);
+const store = createStore(productReducer, applyMiddleware(logger));
 store.subscribe(() => {
   console.log(store.getState());
 });
 
+store.dispatch(getProducts());
 store.dispatch(addProduct("oil"));
-
-store.dispatch(addCart("banana"));
